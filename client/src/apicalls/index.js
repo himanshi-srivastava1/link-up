597f1d71a57ffa0d39324c5657fc2a1502496a5a
himanshi-srivastava1 <himansri04@gmail.com>
2025-12-29 22:47:23 +0530

    Remove sensitive config file from tracking

--- client/src/apicalls/index.js
+++ client/src/apicalls/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-export const url="https://link-up-server.onrender.com";
+export const url="https://localhost:3000";
 export const axiosInstance=axios.create({
     headers:{
         authorization:`Bearer ${localStorage.getItem('token')}`
