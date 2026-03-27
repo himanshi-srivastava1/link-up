686ae9150a149fb04ce7aaa55b9adf3973dfa022
himanshi-srivastava1 <himansri04@gmail.com>
2026-03-27 20:26:07 +0530

    c1

--- server/config/email.js
+++ server/config/email.js
@@ -18,18 +18,21 @@ class EmailService {
             this.transporter = nodemailer.createTransport({
                 host: 'smtp.gmail.com',
                 port: 465, 
-                secure: true,
+                secure: true, 
                 auth: {
                     user: process.env.EMAIL_USER,
                     pass: process.env.EMAIL_PASS
                 },
                 tls: {
-                    servername: 'smtp.gmail.com', 
+                    servername: 'smtp.gmail.com',
                     rejectUnauthorized: false
-                }
+                },
+                connectionTimeout: 10000,
+                greetingTimeout: 10000,
+                socketTimeout: 10000
             });
 
-            
+
             await new Promise((resolve, reject) => {
                 this.transporter.verify((error, success) => {
                     if (error) reject(error);
